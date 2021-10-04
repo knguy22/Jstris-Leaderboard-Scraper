b92385b081381e19dc8fa66b5ebb2c63a164c59d
doublesmokedbaconandpie <77951761+doublesmokedbaconandpie@users.noreply.github.com>
2021-10-03 20:46:57 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import consecutivereplays
-import consecutivereplays as replays
-import requests
 import cheese
-import timeit
-import pstats
-import codecs
-import io
 
 
 if __name__ == '__main__':
